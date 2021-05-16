bf571a9fdcb56f26bb8d19e405eaeb333012b3ac
dcarbajal <daniel.carbajal@pucp.edu.pe>
2021-05-16 12:27:42 -0500

    Adding nodemon

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,11 @@ app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 } );
 
+app.get( "/bye", ( req, res ) => {
+    res.send( "Bye world!" );
+} );
+
+
 app.listen( enviroment.serverPort, () => {
     console.log( `Server started at http://localhost:${ enviroment.serverPort }` );
 } );
